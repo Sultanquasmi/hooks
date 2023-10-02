109ee58aef4582d4bd1db94360bb316db3cbe758
Sultanquasmi <67sajid231khan@gmail.com>
2023-10-02 13:36:40 +0530

    first commit

---
+++ usestatehooks/src/App.js
@@ -0,0 +1,12 @@
+import React  from 'react'
+import "./App.css"
+import ReactHooks from './components/ReactHooks'
+
+const App = () => {
+  return (
+    <ReactHooks/>
+  )
+ 
+}
+
+export default App
---
+++ usestatehooks/src/components/ReactHooks.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import React  from 'react'
+
+
+const ReactHooks = () => {
+  
+    const myArray = [
+        {
+            id:1 , Name:'Sultan' , Age:23
+        },
+        {
+            id:2 , Name:'Rohan' , Age:20
+        },
+        {
+            id:3 , Name:'Lakhan' , Age:21
+        },
+        {
+            id:4 , Name:'Suyash' , Age:25
+        }
+    ]
+    const [myValue ,setMyValue ] = useState( myArray)
+    const vanished = (()=>{
+        setMyValue([]);
+    })
+
+  return (
+    <>
+   {
+    myValue.map((currElem)=>{
+        return <h1 className='hstyle'>Name:{currElem.Name} & Age:{currElem.Age}</h1>
+    })
+   }
+   <button className='btn' onClick={vanished}>Click me to vanish</button>
+   </>
+  )
+}
+
+export default ReactHooks
